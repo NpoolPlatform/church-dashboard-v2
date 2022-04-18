7657dabad6b0a4fc4754f2eaa7f742eff31f153d
Zhao KK <kikakkz@hotmail.com>
2022-04-18 18:46:39 +0800

    Add country

--- src/components/internationalization/Country.vue
+++ src/components/internationalization/Country.vue
@@ -7,19 +7,61 @@
     row-key='ID'
     :loading='countryLoading'
     :rows-per-page-options='[20]'
-  />
+    @row-click='(evt, row, index) => onRowClick(row as Country)'
+  >
+    <template #top-right>
+      <div class='row indent flat'>
+        <q-btn
+          dense
+          flat
+          class='btn flat'
+          :label='$t("MSG_CREATE")'
+          @click='onCreate'
+        />
+      </div>
+    </template>
+  </q-table>
+  <q-dialog
+    v-model='showing'
+    @hide='onMenuHide'
+    position='right'
+  >
+    <q-card class='popup-menu'>
+      <q-card-section>
+        <span>{{ $t('MSG_CREATE_COUNTRY') }}</span>
+      </q-card-section>
+      <q-card-section>
+        <q-input v-model='target.Country' :label='$t("MSG_COUNTRY")' />
+        <q-input v-model='target.Flag' :label='$t("MSG_FLAG")' />
+        <q-input v-model='target.Code' :label='$t("MSG_COUNTRY_CODE")' />
+        <q-input v-model='target.Short' :label='$t("MSG_COUNTRY_SHORT")' />
+      </q-card-section>
+      <q-item class='row'>
+        <q-btn class='btn round alt' :label='$t("MSG_SUBMIT")' @click='onSubmit' />
+        <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
+      </q-item>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup lang='ts'>
 import {
   NotificationType,
-  useLangStore
+  useLangStore,
+  Country,
+  useChurchLangStore
 } from 'npool-cli-v2'
 import { computed, onMounted, ref } from 'vue'
 
 const lang = useLangStore()
 const countries = computed(() => lang.Countries)
 const countryLoading = ref(true)
+
+const churchLang = useChurchLangStore()
+
+const showing = ref(false)
+const updating = ref(false)
+const target = ref({} as unknown as Country)
 
 onMounted(() => {
   lang.getCountries({
@@ -35,5 +77,43 @@ onMounted(() => {
     countryLoading.value = false
   })
 })
+
+const onMenuHide = () => {
+  showing.value = false
+  target.value = {} as unknown as Country
+}
+
+const onRowClick = (country: Country) => {
+  target.value = country
+  showing.value = true
+  updating.value = true
+}
+
+const onCreate = () => {
+  showing.value = true
+  updating.value = false
+}
+
+const onSubmit = () => {
+  showing.value = false
+
+  churchLang.createCountry({
+    Info: target.value,
+    Message: {
+      Error: {
+        Title: 'MSG_CREATE_COUNTRY',
+        Message: 'MSG_CREATE_COUNTRY_FAIL',
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  }, () => {
+    // TODO
+  })
+}
+
+const onCancel = () => {
+  showing.value = false
+}
 
 </script>
