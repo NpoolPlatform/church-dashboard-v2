86f2ee1806bdf71291d0c268f68211cc4c788872
Zhao KK <kikakkz@hotmail.com>
2022-04-22 00:54:47 +0800

    Add coinname

---
+++ src/components/limit/AutoReview.vue
@@ -0,0 +1,179 @@
+<template>
+  <q-table
+    dense
+    flat
+    :rows='settings'
+    row-key='ID'
+    :rows-per-page-options='[10]'
+    @row-click='(evt, row, index) => onRowClick(row as WithdrawSetting)'
+  >
+    <template #top-right>
+      <div class='row indent flat'>
+        <q-btn
+          dense
+          flat
+          class='btn flat'
+          :label='$t("MSG_CREATE")'
+          @click='onCreate'
+        />
+      </div>
+    </template>
+  </q-table>
+  <q-dialog
+    v-model='showing'
+    @hide='onMenuHide'
+    position='right'
+  >
+    <q-card class='popup-menu'>
+      <q-card-section>
+        <span>{{ $t('MSG_CREATE_WITHDRAW_SETTING') }}</span>
+      </q-card-section>
+      <q-card-section>
+        <q-select :options='coins' v-model='selectedCoin' :label='$t("MSG_COIN_TYPE")' />
+        <q-input
+          type='number'
+          v-model='target.WithdrawAutoReviewCoinAmount'
+          :label='$t("MSG_WITHDRAW_AUTH_REVIEW_AMOUNT")'
+          :suffix='selectedCoin.value?.Unit'
+        />
+      </q-card-section>
+      <q-item class='row'>
+        <q-btn class='btn round alt' :label='$t("MSG_SUBMIT")' @click='onSubmit' />
+        <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
+      </q-item>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang='ts'>
+import { Coin, NotificationType, useChurchWithdrawettingStore, useCoinStore, WithdrawSetting } from 'npool-cli-v2'
+import { useLocalApplicationStore } from 'src/localstore'
+import { computed, onMounted, ref } from 'vue'
+
+const app = useLocalApplicationStore()
+const appID = computed(() => app.AppID)
+
+interface MySetting extends WithdrawSetting {
+  CoinName: string
+}
+
+const setting = useChurchWithdrawettingStore()
+const appSettings = computed(() => setting.WithdrawSettings.get(appID.value) ? setting.WithdrawSettings.get(appID.value) : [])
+const settings = computed(() => Array.from(appSettings.value as Array<WithdrawSetting>).map((el) => {
+  const ms = el as MySetting
+  ms.CoinName = coin.getCoinByID(ms.CoinTypeID)?.Name as string
+  return ms
+}))
+
+interface MyCoin {
+  label: string
+  value: Coin
+}
+
+const coin = useCoinStore()
+const coins = computed(() => Array.from(coin.Coins.filter((coin) => !coin.PreSale)).map((el) => {
+  return {
+    label: el.Name,
+    value: el
+  } as MyCoin
+}))
+const selectedCoin = computed({
+  get: () => {
+    return {
+      label: coin.getCoinByID(target.value?.CoinTypeID)?.Name,
+      value: coin.getCoinByID(target.value?.CoinTypeID)
+    } as MyCoin
+  },
+  set: (val) => {
+    target.value.CoinTypeID = val.value.ID as string
+  }
+})
+
+onMounted(() => {
+  setting.getWithdrawSettings({
+    TargetAppID: appID.value,
+    Message: {
+      Error: {
+        Title: 'MSG_GET_WITHDRAW_SETTINGS',
+        Message: 'MSG_GET_WITHDRAW_SETTINGS_FAIL',
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  }, () => {
+    // TODO
+  })
+
+  coin.getCoins({
+    Message: {
+      Error: {
+        Title: 'MSG_GET_COINS',
+        Message: 'MSG_GET_COINS_FAIL',
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  }, () => {
+    // TODO
+  })
+})
+
+const showing = ref(false)
+const updating = ref(false)
+const target = ref({} as unknown as WithdrawSetting)
+
+const onCreate = () => {
+  showing.value = true
+  updating.value = false
+}
+
+const onRowClick = (s: WithdrawSetting) => {
+  showing.value = true
+  updating.value = true
+  target.value = s
+}
+
+const onMenuHide = () => {
+  showing.value = false
+  target.value = {} as unknown as WithdrawSetting
+}
+
+const onSubmit = () => {
+  showing.value = false
+  if (updating.value) {
+    setting.updateWithdrawSetting({
+      Info: target.value,
+      Message: {
+        Error: {
+          Title: 'MSG_UPDATE_WITHDRAW_SETTINGS',
+          Message: 'MSG_UPDATE_WITHDRAW_SETTINGS_FAIL',
+          Popup: true,
+          Type: NotificationType.Error
+        }
+      }
+    }, () => {
+      // TODO
+    })
+    return
+  }
+  setting.createWithdrawSetting({
+    TargetAppID: appID.value,
+    Info: target.value,
+    Message: {
+      Error: {
+        Title: 'MSG_CREATE_WITHDRAW_SETTINGS',
+        Message: 'MSG_CREATE_WITHDRAW_SETTINGS_FAIL',
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  }, () => {
+    // TODO
+  })
+}
+
+const onCancel = () => {
+  onMenuHide()
+}
+
+</script>
