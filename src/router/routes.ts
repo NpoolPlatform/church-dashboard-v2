82495f217c0bc1c1aa66cb901515381730d913b3
Zhao KK <kikakkz@hotmail.com>
2022-04-23 12:39:23 +0800

    Add airdrop

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -612,6 +612,18 @@ const routes: RouteRecordRaw[] = [
           ShowTopTip: false,
           NeedLogined: true
         }
+      }, {
+        path: 'inspire/airdrop',
+        component: () => import('pages/inspire/AirDrop.vue'),
+        meta: {
+          ShowHeaderAnnouncement: true,
+          ShowMainHeader: false,
+          ShowBigLogo: true,
+          ShowFooterTop: true,
+          ShowSignHelper: true,
+          ShowTopTip: false,
+          NeedLogined: true
+        }
       }
     ]
   },
