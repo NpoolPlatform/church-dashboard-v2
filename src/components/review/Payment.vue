3c924898e883e96f7f62184946994beae0f271e5
Zhao KK <kikakkz@hotmail.com>
2022-05-31 11:37:52 +0800

    Better user payment balance

--- src/components/review/Payment.vue
+++ src/components/review/Payment.vue
@@ -12,7 +12,7 @@
     dense
     flat
     :title='$t("MSG_REVIEW_USER_PAYMENT_BALANCES")'
-    :rows='balances'
+    :rows='myBalances'
     row-key='ID'
     :loading='balanceLoading'
     :rows-per-page-options='[10]'
@@ -20,7 +20,7 @@
 </template>
 
 <script setup lang='ts'>
-import { NotificationType, useChurchBillingStore } from 'npool-cli-v2'
+import { NotificationType, Payment, useChurchBillingStore, useCoinStore, UserPaymentBalance } from 'npool-cli-v2'
 import { useLocalApplicationStore } from 'src/localstore'
 import { computed, onMounted, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -32,8 +32,35 @@ const app = useLocalApplicationStore()
 const appID = computed(() => app.AppID)
 
 const billing = useChurchBillingStore()
-const payments = computed(() => billing.Payments.get(appID.value))
+const payments = computed(() => billing.Payments.get(appID.value) as Array<Payment>)
 const balances = computed(() => billing.PaymentBalances.get(appID.value))
+
+interface MyBalance extends UserPaymentBalance {
+  CoinName: string
+  CoinUSDCurrency: number
+  USDAmount: number
+}
+const myBalances = computed(() => {
+  const bls = [] as Array<MyBalance>
+  balances.value?.forEach((balance) => {
+    const el = balance as MyBalance
+    const index = payments.value?.findIndex((pay) => pay.ID === el.PaymentID)
+    if (index === undefined) {
+      return
+    }
+    if (index < 0) {
+      return
+    }
+    const payment = payments.value[index]
+    el.CoinName = coin.getCoinByID(payment.CoinInfoID)?.Name as string
+    el.CoinUSDCurrency = payment.CoinUSDCurrency
+    el.USDAmount = el.Amount * payment.CoinUSDCurrency
+    bls.push(el)
+  })
+  return bls
+})
+
+const coin = useCoinStore()
 
 const paymentLoading = ref(false)
 const balanceLoading = ref(false)
@@ -77,6 +104,19 @@ watch(appID, () => {
 
 onMounted(() => {
   prepare()
+
+  coin.getCoins({
+    Message: {
+      Error: {
+        Title: 'MSG_GET_COINS',
+        Message: 'MSG_GET_COINS_FAIL',
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  }, () => {
+    // TODO
+  })
 })
 
 </script>
