831978f08c725640fab9633ad58bca82cfbbb4cd
Zhao KK <kikakkz@hotmail.com>
2022-06-05 20:52:49 +0800

    Add payment information for not idle payment address

--- src/components/account/Payment.vue
+++ src/components/account/Payment.vue
@@ -10,8 +10,20 @@
 </template>
 
 <script setup lang='ts'>
-import { useChurchAccountStore, useCoinStore, NotificationType, GoodPayment } from 'npool-cli-v2'
+import {
+  useChurchAccountStore,
+  useCoinStore,
+  NotificationType,
+  GoodPayment,
+  PaymentState,
+  useChurchBillingStore,
+  InvalidID
+} from 'npool-cli-v2'
+import { useLocalApplicationStore } from 'src/localstore'
 import { computed, onMounted } from 'vue'
+
+const app = useLocalApplicationStore()
+const appID = computed(() => app.AppID)
 
 const account = useChurchAccountStore()
 const coin = useCoinStore()
@@ -19,12 +31,37 @@ const coin = useCoinStore()
 interface MyAccount extends GoodPayment {
   CoinName: string
   Address: string
+  PaymentState: PaymentState
+  PaymentID: string
+  OrderID: string
+  UserPaymentBalanceID: string
 }
+
+const billing = useChurchBillingStore()
+const payments = computed(() => billing.Payments.get(appID.value)?.filter((el) => el.State === PaymentState.TIMEOUT))
+const balances = computed(() => billing.PaymentBalances.get(appID.value))
 
 const accounts = computed(() => Array.from(account.GoodPayments).map((el) => {
   const ac = el as MyAccount
   ac.Address = account.getAccountByID(ac.AccountID)?.Address
   ac.CoinName = coin.getCoinByID(account.getAccountByID(ac.AccountID)?.CoinTypeID)?.Name as string
+  ac.PaymentState = 'MSG_NOT_AVAILABLE' as PaymentState
+  ac.PaymentID = InvalidID
+  ac.UserPaymentBalanceID = InvalidID
+  if (payments.value) {
+    const index = payments.value.findIndex((pel) => pel.AccountID === el.AccountID && !el.Idle)
+    if (index >= 0) {
+      ac.PaymentState = payments.value[index].State as PaymentState
+      ac.PaymentID = payments.value[index].ID
+      ac.OrderID = payments.value[index].OrderID
+    }
+  }
+  if (balances.value) {
+    const index = balances.value.findIndex((bel) => bel.PaymentID === ac.PaymentID)
+    if (index >= 0) {
+      ac.UserPaymentBalanceID = balances.value[index].ID as string
+    }
+  }
   return ac
 }))
 
