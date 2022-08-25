0f18d49d79359671a2682df183dac254c2e40dcc
daqiang.li <dqli161@163.com>
2022-08-25 20:00:40 +0800

    Display app selection

--- src/components/header/HeaderTools.vue
+++ src/components/header/HeaderTools.vue
@@ -30,12 +30,13 @@
 </template>
 
 <script setup lang='ts'>
-import { useApplicationsStore, useLoginedUserStore, useMailboxStore, Application, NotificationType } from 'npool-cli-v2'
+import { useApplicationsStore, useMailboxStore, Application, NotificationType } from 'npool-cli-v2'
 import { defineAsyncComponent, computed, watch, onMounted } from 'vue'
 import { useLocalApplicationStore } from 'src/localstore'
 
 import bellNoMsg from '../../assets/bell-no-msg.svg'
 import bellMsg from '../../assets/bell-msg.svg'
+import { useLocalUserStore } from 'npool-cli-v4'
 
 const tracing = 'https://www.jaegertracing.io/img/jaeger-icon-reverse-color.svg'
 const github = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png'
@@ -44,8 +45,8 @@ const AvatarDropdown = defineAsyncComponent(() => import('src/components/avatar/
 const HeaderToolBtn = defineAsyncComponent(() => import('src/components/header/HeaderToolBtn.vue'))
 const LangSwitcher = defineAsyncComponent(() => import('src/components/lang/LangSwitcher.vue'))
 
-const loginedUser = useLoginedUserStore()
-const logined = computed(() => loginedUser.getLogined)
+const loginedUser = useLocalUserStore()
+const logined = computed(() => loginedUser.logined)
 
 const application = useApplicationsStore()
 const localapplication = useLocalApplicationStore()
