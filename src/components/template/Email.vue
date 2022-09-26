ef6c84ccf9fc970960d9be04abea8c218977772d
lidaqiang <dqli161@163.com>
2022-09-26 13:33:21 +0800

    Disable Selection When Updating

--- src/components/template/Email.vue
+++ src/components/template/Email.vue
@@ -37,7 +37,7 @@
       </q-card-section>
       <q-card-section>
         <q-input v-model='target.DefaultToUsername' :label='$t("MSG_DEFAULT_TO_USERNAME")' />
-        <q-select :options='UsedFors' v-model='target.UsedFor' :label='$t("MSG_USED_FOR")' />
+        <q-select :options='UsedFors' v-model='target.UsedFor' :disable='updating' :label='$t("MSG_USED_FOR")' />
         <q-input v-model='target.Sender' :label='$t("MSG_SENDER")' />
         <q-input v-model='replyTos' :label='$t("MSG_REPLY_TOS_COMMA")' />
         <q-input v-model='ccTos' :label='$t("MSG_CC_TOS_COMMA")' />
