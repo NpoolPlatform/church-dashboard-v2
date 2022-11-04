60c29b11955f2c861f357580b664d18d71d73637
lidaqiang <dqli161@163.com>
2022-11-04 18:52:09 +0800

    Add GasFeeder

--- src/components/account/GasFeeder.vue
+++ src/components/account/GasFeeder.vue
@@ -3,10 +3,10 @@
     dense
     flat
     :title='$t("MSG_COIN_GASES")'
-    :rows='gases'
+    :rows='gasProviders'
     row-key='ID'
     :rows-per-page-options='[10]'
-    @row-click='(evt, row, index) => onRowClick(row as CoinGas)'
+    @row-click='(evt, row, index) => onRowClick(row as PlatformAccount)'
   >
     <template #top-right>
       <div class='row indent flat'>
@@ -20,14 +20,6 @@
       </div>
     </template>
   </q-table>
-  <q-table
-    dense
-    flat
-    :title='$t("MSG_COIN_GAS_DEPOSITS")'
-    :rows='deposits'
-    row-key='ID'
-    :rows-per-page-options='[10]'
-  />
   <q-dialog
     v-model='showing'
     @hide='onMenuHide'
@@ -38,25 +30,7 @@
         <span>{{ $t('MSG_CREATE_COIN_GAS') }}</span>
       </q-card-section>
       <q-card-section>
-        <q-select :options='coins' v-model='selectedCoin' :label='$t("MSG_COIN_TYPE")' />
-        <q-select :options='coins' v-model='selectedGasCoin' :label='$t("MSG_GAS_COIN_TYPE")' />
-        <q-input
-          type='number'
-          v-model='target.DepositThresholdLow'
-          :label='$t("MSG_DEPOSIT_THRESHOLD_LOW")'
-          :suffix='selectedGasCoin?.value?.Unit'
-        />
-        <q-input
-          type='number'
-          v-model='target.DepositAmount'
-          :label='$t("MSG_DEPOSIT_AMOUNT")'
-          :suffix='selectedGasCoin?.value?.Unit'
-        />
-        <q-input
-          type='number'
-          v-model='target.OnlineScale'
-          :label='$t("MSG_ONLINE_SCALE")'
-        />
+        <!-- <q-select :options='coins' v-model='selectedGasCoin' :label='$t("MSG_GAS_COIN_TYPE")' /> -->
       </q-card-section>
       <q-item class='row'>
         <q-btn class='btn round alt' :label='$t("MSG_SUBMIT")' @click='onSubmit' />
@@ -67,148 +41,78 @@
 </template>
 
 <script setup lang='ts'>
-import { useCoinStore, NotificationType, useChurchGasFeederStore, Coin, CoinGas } from 'npool-cli-v2'
+import { AccountUsedFor, PlatformAccount, useChurchPlatformAccountStore } from 'npool-cli-v4'
+import { getPlatformAccounts } from 'src/api/account'
 import { computed, onMounted, ref } from 'vue'
 
-const feeder = useChurchGasFeederStore()
-const gases = computed(() => feeder.CoinGases)
-const deposits = computed(() => feeder.Deposits)
-const coin = useCoinStore()
+const platform = useChurchPlatformAccountStore()
+const gasProviders = computed(() => platform.getAccountsByKey(AccountUsedFor.GasProvider))
 
 const showing = ref(false)
 const updating = ref(false)
-const target = ref({} as unknown as CoinGas)
-
-interface MyCoin {
-  label: string
-  value: Coin
-}
-
-const coins = computed(() => Array.from(coin.Coins).map((el) => {
-  return {
-    label: el.Name,
-    value: el
-  } as MyCoin
-}))
-const selectedCoin = computed({
-  get: () => {
-    return {
-      label: coin.getCoinByID(target.value.CoinTypeID)?.Name,
-      value: coin.getCoinByID(target.value.CoinTypeID)
-    } as MyCoin
-  },
-  set: (val) => {
-    target.value.CoinTypeID = val.value.ID as string
-  }
-})
-const selectedGasCoin = computed({
-  get: () => {
-    return {
-      label: coin.getCoinByID(target.value.GasCoinTypeID)?.Name,
-      value: coin.getCoinByID(target.value.GasCoinTypeID)
-    } as MyCoin
-  },
-  set: (val) => {
-    target.value.GasCoinTypeID = val.value.ID as string
-  }
-})
+const target = ref({} as PlatformAccount)
 
 const onCreate = () => {
   showing.value = true
   updating.value = false
-  target.value = {} as unknown as CoinGas
+  target.value = {} as PlatformAccount
 }
 
 const onMenuHide = () => {
   showing.value = false
   updating.value = false
-  target.value = {} as unknown as CoinGas
+  target.value = {} as PlatformAccount
 }
 
-const onRowClick = (gas: CoinGas) => {
+const onRowClick = (gas: PlatformAccount) => {
   showing.value = true
   updating.value = true
   target.value = gas
-}
-
-const onSubmit = () => {
-  showing.value = false
-
-  if (updating.value) {
-    feeder.updateCoinGas({
-      Info: target.value,
-      Message: {
-        Error: {
-          Title: 'MSG_UPDATE_COIN_GAS',
-          Message: 'MSG_UPDATE_COIN_GAS_FAIL',
-          Popup: true,
-          Type: NotificationType.Error
-        }
-      }
-    }, () => {
-      // TODO
-    })
-    return
-  }
-
-  feeder.createCoinGas({
-    Info: target.value,
-    Message: {
-      Error: {
-        Title: 'MSG_CREATE_COIN_GAS',
-        Message: 'MSG_CREATE_COIN_GAS_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
 }
 
 const onCancel = () => {
   onMenuHide()
 }
 
+const onSubmit = () => {
+  showing.value = false
+
+  // if (updating.value) {
+  //   feeder.updatePlatformAccount({
+  //     Info: target.value,
+  //     Message: {
+  //       Error: {
+  //         Title: 'MSG_UPDATE_COIN_GAS',
+  //         Message: 'MSG_UPDATE_COIN_GAS_FAIL',
+  //         Popup: true,
+  //         Type: NotificationType.Error
+  //       }
+  //     }
+  //   }, () => {
+  //     // TODO
+  //   })
+  //   return
+  // }
+
+  // feeder.createPlatformAccount({
+  //   Info: target.value,
+  //   Message: {
+  //     Error: {
+  //       Title: 'MSG_CREATE_COIN_GAS',
+  //       Message: 'MSG_CREATE_COIN_GAS_FAIL',
+  //       Popup: true,
+  //       Type: NotificationType.Error
+  //     }
+  //   }
+  // }, () => {
+  //   // TODO
+  // })
+}
+
 onMounted(() => {
-  coin.getCoins({
-    Message: {
-      Error: {
-        Title: 'MSG_GET_COINS',
-        Message: 'MSG_GET_COINS_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
-
-  feeder.getCoinGases({
-    Message: {
-      Error: {
-        Title: 'MSG_GET_COIN_GASES',
-        Message: 'MSG_GET_COIN_GASES_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
-
-  feeder.getDeposits({
-    Message: {
-      Error: {
-        Title: 'MSG_GET_DEPOSITS',
-        Message: 'MSG_GET_DEPOSITS_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
+  if (platform.PlatformAccounts.PlatformAccounts.length === 0) {
+    getPlatformAccounts(0, 500)
+  }
 })
 
 </script>
