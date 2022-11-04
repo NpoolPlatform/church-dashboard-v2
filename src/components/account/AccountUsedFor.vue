e8437d126fb3d92bd669a5b155458a3d96178b02
lidaqiang <dqli161@163.com>
2022-11-04 19:05:38 +0800

    useful component

---
+++ src/components/account/AccountUsedFor.vue
@@ -0,0 +1,121 @@
+<template>
+  <q-dialog
+    v-model='showing'
+    @hide='onMenuHide'
+    position='right'
+  >
+    <q-card class='popup-menu'>
+      <q-card-section>
+        <span>{{ $t('MSG_CREATE_APPLICATION') }}</span>
+      </q-card-section>
+      <q-card-section v-if='!updating'>
+        <CoinPicker v-model:coin='target.CoinTypeID' />
+        <q-input v-model='target.Address' :label='$t("MSG_ADDRESS")' />
+        <q-select :options='AccountUsedFors' v-model='target.UsedFor' disable :label='$t("MSG_ACCOUNT_USED_FOR")' />
+      </q-card-section>
+      <q-card-section v-if='updating'>
+        <div>
+          <q-toggle dense v-model='target.Backup' :label='$t("MSG_BACKUP")' />
+        </div>
+        <div>
+          <q-toggle dense v-model='target.Blocked' :label='$t("MSG_BLOCKED")' />
+        </div>
+        <div>
+          <q-toggle dense v-model='target.Active' :label='$t("MSG_ACTIVE")' />
+        </div>
+        <div>
+          <q-toggle dense v-model='target.Locked' :label='$t("MSG_LOCKED")' />
+        </div>
+      </q-card-section>
+      <q-item class='row'>
+        <LoadingButton loading :label='$t("MSG_SUBMIT")' @click='onSubmit' />
+        <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
+      </q-item>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang='ts'>
+import { AccountUsedFor, AccountUsedFors, NotifyType, PlatformAccount, useChurchPlatformAccountStore } from 'npool-cli-v4'
+import { computed, defineAsyncComponent, ref } from 'vue'
+
+const CoinPicker = defineAsyncComponent(() => import('src/components/coin/CoinPicker.vue'))
+const LoadingButton = defineAsyncComponent(() => import('src/components/button/LoadingButton.vue'))
+
+const platform = useChurchPlatformAccountStore()
+
+const target = ref({} as PlatformAccount)
+const showing = ref(false)
+const updating = ref(false)
+
+const onMenuHide = () => {
+  showing.value = false
+  target.value = { UsedFor: AccountUsedFor.PlatformBenefitCold } as PlatformAccount
+}
+
+const onCancel = () => {
+  onMenuHide()
+}
+
+const onSubmit = (done: () => void) => {
+  updating.value ? updatePlatformAccount(done) : createPlatformAccount(done)
+}
+
+const updateTarget = computed(() => {
+  return {
+    ID: target.value.ID,
+    Backup: target.value.Backup,
+    Active: target.value.Active,
+    Blocked: target.value.Blocked,
+    Locked: target.value.Locked
+  }
+})
+
+const updatePlatformAccount = (done: () => void) => {
+  platform.updatePlatformAccount({
+    ...updateTarget.value,
+    Message: {
+      Error: {
+        Title: 'MSG_UPDATE_PLATFORM_ACCOUNT',
+        Popup: true,
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_UPDATE_PLATFORM_ACCOUNT',
+        Popup: true,
+        Type: NotifyType.Success
+      }
+    }
+  }, (account: PlatformAccount, error: boolean) => {
+    done()
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
+}
+
+const createPlatformAccount = (done: () => void) => {
+  platform.createPlatformAccount({
+    ...target.value,
+    Message: {
+      Error: {
+        Title: 'MSG_CREATE_PLATFORM_ACCOUNT',
+        Popup: true,
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_CREATE_PLATFORM_ACCOUNT',
+        Popup: true,
+        Type: NotifyType.Success
+      }
+    }
+  }, (account: PlatformAccount, error: boolean) => {
+    done()
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
+}
+</script>
