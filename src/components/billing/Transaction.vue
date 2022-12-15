c7095823b5166a2e75307a99f2c38fa3475581aa
lidaqiang <dqli161@163.com>
2022-12-15 10:45:01 +0800

    bugfix: #1047

--- src/components/billing/Transaction.vue
+++ src/components/billing/Transaction.vue
@@ -12,24 +12,16 @@
 <script setup lang='ts'>
 import { useChurchTxStore } from 'npool-cli-v4'
 import { getTxs } from 'src/api/chain'
-import { useLocalApplicationStore } from 'src/localstore'
-import { computed, onMounted, watch } from 'vue'
-
-const app = useLocalApplicationStore()
-const appID = computed(() => app.AppID)
+import { computed, onMounted } from 'vue'
 
 const tx = useChurchTxStore()
-const transactions = computed(() => tx.getTxsByAppID(appID.value))
+const transactions = computed(() => tx.Txs.Txs)
 
 const prepare = () => {
   if (transactions.value.length === 0) {
     getTxs(0, 500)
   }
 }
-
-watch(appID, () => {
-  prepare()
-})
 
 onMounted(() => {
   prepare()
