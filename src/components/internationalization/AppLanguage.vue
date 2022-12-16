cfabe2fa802ca13a43b25929bbcadcab9a0a5e14
lidaqiang <dqli161@163.com>
2022-12-16 16:30:44 +0800

    add language

---
+++ src/components/internationalization/AppLanguage.vue
@@ -0,0 +1,153 @@
+<template>
+  <q-table
+    dense
+    flat
+    :title='$t("MSG_APP_LANGUAGES")'
+    :rows='langs'
+    row-key='ID'
+    :rows-per-page-options='[20]'
+    :selected='selectedAppLangs'
+    selection='single'
+  >
+    <template #top-right>
+      <div class='row indent flat'>
+        <q-btn
+          dense
+          flat
+          class='btn flat'
+          :label='$t("MSG_CREATE")'
+          @click='onCreate'
+        />
+        <q-btn
+          dense
+          flat
+          class='btn flat'
+          :label='$t("MSG_DELETE")'
+          :disable='selectedAppLangs.length === 0'
+          @click='onDelete'
+        />
+      </div>
+    </template>
+  </q-table>
+  <q-dialog
+    v-model='showing'
+    @hide='onMenuHide'
+    position='right'
+  >
+    <q-card class='popup-menu'>
+      <q-card-section>
+        <span>{{ $t('MSG_CREATE_APP_LANGUAGE') }}</span>
+      </q-card-section>
+      <q-card-selection>
+        <LanguagePicker v-model:id='target.LangID' />
+      </q-card-selection>
+      <q-card-section>
+        <div><q-toggle dense v-model='target.Main' :label='$t("MSG_MAIN_LANGUAGE")' /></div>
+      </q-card-section>
+      <q-item class='row'>
+        <LoadingButton loading :label='$t("MSG_AUTHORIZE")' @click='onAuthorize' />
+        <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
+      </q-item>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang='ts'>
+import { NotifyType } from 'npool-cli-v4'
+import { appID } from 'src/api/app'
+import { getAppLangs } from 'src/api/g11n'
+import { useChurchAppLangStore } from 'src/teststore/g11n/applang'
+import { AppLang } from 'src/teststore/g11n/applang/types'
+import { computed, defineAsyncComponent, onMounted, ref, watch } from 'vue'
+
+const LoadingButton = defineAsyncComponent(() => import('src/components/button/LoadingButton.vue'))
+const LanguagePicker = defineAsyncComponent(() => import('src/components/internationalization/LanguagePicker.vue'))
+
+const lang = useChurchAppLangStore()
+const langs = computed(() => lang.getAppLangsByAppID(appID.value))
+
+const selectedAppLangs = ref([] as Array<AppLang>)
+const target = ref({} as AppLang)
+
+const showing = ref(false)
+const updating = ref(false)
+
+const onCreate = () => {
+  showing.value = true
+  updating.value = false
+}
+
+const onMenuHide = () => {
+  showing.value = false
+}
+
+const onCancel = () => {
+  onMenuHide()
+}
+
+const onAuthorize = (done: () => void) => {
+  lang.createAppLang({
+    TargetAppID: appID.value,
+    TargetLangID: target.value?.LangID,
+    Main: target.value?.Main,
+    Message: {
+      Error: {
+        Title: 'MSG_CREATE_APP_LANGUAGE',
+        Message: 'MSG_CREATE_APP_LANGUAGE_FAIL',
+        Popup: true,
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_CREATE_APP_LANGUAGE',
+        Message: 'MSG_CREATE_APP_LANGUAGE_SUCCESS',
+        Popup: true,
+        Type: NotifyType.Success
+      }
+    }
+  }, (error: boolean) => {
+    done()
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
+}
+
+const onDelete = () => {
+  lang.deleteAppLang({
+    ID: selectedAppLangs.value[0]?.ID,
+    TargetAppID: selectedAppLangs.value[0]?.AppID,
+    Message: {
+      Error: {
+        Title: 'MSG_DELETE_APP_LANGUAGE',
+        Message: 'MSG_DELETE_APP_LANGUAGE_FAIL',
+        Popup: true,
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_DELETE_APP_LANGUAGE',
+        Message: 'MSG_DELETE_APP_LANGUAGE_FAIL',
+        Popup: true,
+        Type: NotifyType.Success
+      }
+    }
+  }, (error: boolean) => {
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
+}
+
+watch(appID, () => {
+  if (langs.value.length === 0) {
+    getAppLangs(0, 100)
+  }
+})
+
+onMounted(() => {
+  if (langs.value.length === 0) {
+    getAppLangs(0, 100)
+  }
+})
+</script>
--- src/components/internationalization/Languages.vue
+++ src/components/internationalization/Languages.vue
@@ -5,9 +5,8 @@
     :title='$t("MSG_LANGUAGES")'
     :rows='langs'
     row-key='ID'
-    :loading='langLoading'
     :rows-per-page-options='[20]'
-    @row-click='(evt, row, index) => onRowClick(row as Language)'
+    @row-click='(evt, row, index) => onRowClick(row as Lang)'
   >
     <template #top-right>
       <div class='row indent flat'>
@@ -16,15 +15,15 @@
           flat
           class='btn flat'
           :label='$t("MSG_CREATE")'
-          @click='onAdd'
+          @click='onCreate'
         />
-        <q-btn
+        <!-- <q-btn
           dense
           flat
           class='btn flat'
           :label='$t("MSG_EXPORT")'
           @click='onExport'
-        />
+        /> -->
       </div>
     </template>
   </q-table>
@@ -35,7 +34,7 @@
   >
     <q-card class='popup-menu'>
       <q-card-section>
-        <span>{{ $t('MSG_CREATE_LANGUAGE') }}</span>
+        <span>{{ updating? $t('MSG_UPDATE_LANGUAGE') : $t('MSG_CREATE_LANGUAGE') }}</span>
       </q-card-section>
       <q-card-section>
         <q-input v-model='target.Name' :label='$t("MSG_LANGUAGE_NAME")' />
@@ -44,98 +43,113 @@
         <q-input v-model='target.Short' :label='$t("MSG_LANGUAGE_SHORT")' />
       </q-card-section>
       <q-item class='row'>
-        <q-btn class='btn round alt' :label='$t("MSG_SUBMIT")' @click='onSubmit' />
+        <LoadingButton loading :label='$t("MSG_SUBMIT")' @click='onSubmit' />
         <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
       </q-item>
     </q-card>
   </q-dialog>
+  <AppLanguage />
 </template>
 
 <script setup lang='ts'>
-import { Language, NotificationType, useAdminLangStore, useChurchLangStore } from 'npool-cli-v2'
-import { computed, onMounted, ref } from 'vue'
+import { NotifyType } from 'npool-cli-v4'
+import { useChurchLangStore } from 'src/teststore/g11n/lang'
+import { Lang, UpdateLangRequest } from 'src/teststore/g11n/lang/types'
+import { computed, defineAsyncComponent, ref } from 'vue'
 
-const alang = useAdminLangStore()
-const langLoading = ref(true)
+const LoadingButton = defineAsyncComponent(() => import('src/components/button/LoadingButton.vue'))
+const AppLanguage = defineAsyncComponent(() => import('src/components/internationalization/AppLanguage.vue'))
 
-const clang = useChurchLangStore()
-const langs = computed(() => alang.Languages)
+const lang = useChurchLangStore()
+const langs = computed(() => lang.Langs.Langs)
 
-onMounted(() => {
-  alang.getLangs({
-    Message: {
-      Error: {
-        Title: 'MSG_GET_LANGS',
-        Message: 'MSG_GET_LANGS_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    langLoading.value = false
-  })
-})
-
+const target = ref({} as Lang)
 const showing = ref(false)
 const updating = ref(false)
-const target = ref({} as unknown as Language)
 
-const onMenuHide = () => {
-  showing.value = false
-  target.value = {} as unknown as Language
-}
-
-const onAdd = () => {
+const onCreate = () => {
   showing.value = true
   updating.value = false
 }
 
-const onExport = () => {
-  // TODO
-}
-
-const onRowClick = (language: Language) => {
-  target.value = language
-  showing.value = true
-  updating.value = true
-}
-
-const onSubmit = () => {
+const onMenuHide = () => {
+  target.value = {} as Lang
   showing.value = false
-  if (updating.value) {
-    clang.updateLang({
-      Info: target.value,
-      Message: {
-        Error: {
-          Title: 'MSG_UPDATE_LANG',
-          Message: 'MSG_UPDATE_LANG_FAIL',
-          Popup: true,
-          Type: NotificationType.Error
-        }
-      }
-    }, () => {
-      // TODO
-    })
-    return
-  }
+}
 
-  clang.createLang({
-    Info: target.value,
-    Message: {
-      Error: {
-        Title: 'MSG_CREATE_LANG',
-        Message: 'MSG_CREATE_LANG_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
+const onRowClick = (row: Lang) => {
+  target.value = { ...row }
+  updating.value = true
+  showing.value = true
 }
 
 const onCancel = () => {
-  showing.value = false
+  onMenuHide()
 }
 
+const onSubmit = (done: () => void) => {
+  updating.value ? updateLang(done) : createLang(done)
+}
+
+const createLang = (done: () => void) => {
+  lang.createLang({
+    ...target.value,
+    Message: {
+      Error: {
+        Title: 'MSG_CREATE_LANGUAGE',
+        Message: 'MSG_CREATE_LANGUAGE_FAIL',
+        Popup: true,
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_CREATE_LANGUAGE',
+        Message: 'MSG_CREATE_LANGUAGE_SUCCESS',
+        Popup: true,
+        Type: NotifyType.Success
+      }
+    }
+  }, (error: boolean) => {
+    done()
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
+}
+
+const updateTarget = computed(() => {
+  return {
+    ID: target.value?.ID,
+    Lang: target.value?.Lang,
+    Logo: target.value?.Logo,
+    Name: target.value?.Name,
+    Short: target.value?.Short
+  } as UpdateLangRequest
+})
+
+const updateLang = (done: () => void) => {
+  lang.updateLang({
+    ...updateTarget.value,
+    Message: {
+      Error: {
+        Title: 'MSG_UPDATE_LANGUAGE',
+        Message: 'MSG_UPDATE_LANGUAGE_FAIL',
+        Popup: true,
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_UPDATE_LANGUAGE',
+        Message: 'MSG_UPDATE_LANGUAGE_FAIL',
+        Popup: true,
+        Type: NotifyType.Success
+      }
+    }
+  }, (error: boolean) => {
+    done()
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
+}
 </script>
