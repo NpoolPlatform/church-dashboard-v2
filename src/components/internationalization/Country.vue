9db9e0332c0cfe9a055d210e697f2ad00940d5bb
lidaqiang <dqli161@163.com>
2022-12-16 14:58:25 +0800

    Add Countries

--- src/components/internationalization/Country.vue
+++ src/components/internationalization/Country.vue
@@ -5,7 +5,6 @@
     :title='$t("MSG_COUNTRIES")'
     :rows='countries'
     row-key='ID'
-    :loading='countryLoading'
     :rows-per-page-options='[10]'
     @row-click='(evt, row, index) => onRowClick(row as Country)'
   >
@@ -25,6 +24,13 @@
           :label='$t("MSG_EXPORT")'
           @click='onExport'
         />
+        <q-btn
+          color='primary'
+          dense
+          icon='cloud_upload'
+          round
+          @click='upload'
+        />
       </div>
     </template>
   </q-table>
@@ -32,7 +38,6 @@
     dense
     flat
     :title='$t("MSG_LOADED_COUNTRIES")'
-    :rows='loadedCountries'
     row-key='ID'
     :rows-per-page-options='[10]'
   >
@@ -78,7 +83,7 @@
         <q-input v-model='target.Short' :label='$t("MSG_COUNTRY_SHORT")' />
       </q-card-section>
       <q-item class='row'>
-        <q-btn class='btn round alt' :label='$t("MSG_SUBMIT")' @click='onSubmit' />
+        <LoadingButton loading :label='$t("MSG_SUBMIT")' @click='onSubmit' />
         <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
       </q-item>
     </q-card>
@@ -87,105 +92,117 @@
 
 <script setup lang='ts'>
 import { saveAs } from 'file-saver'
-import {
-  NotificationType,
-  useLangStore,
-  Country,
-  useChurchLangStore,
-  formatTime
-} from 'npool-cli-v2'
-import { computed, onMounted, ref } from 'vue'
+import { NotificationType, useChurchLangStore } from 'npool-cli-v2'
+import { NotifyType, formatTime } from 'npool-cli-v4'
+import { useChurchCountryStore } from 'src/teststore/g11n/country'
+import { Country, UpdateCountryRequest } from 'src/teststore/g11n/country/types'
+import { computed, defineAsyncComponent, onMounted, ref } from 'vue'
 
-const lang = useLangStore()
-const countries = computed(() => lang.Countries)
-const countryLoading = ref(true)
+const LoadingButton = defineAsyncComponent(() => import('src/components/button/LoadingButton.vue'))
 
+const country = useChurchCountryStore()
+const countries = computed(() => country.Countries.Countries)
 const loadedCountries = ref([] as Array<Country>)
 
 const churchLang = useChurchLangStore()
 
+const target = ref({} as Country)
 const showing = ref(false)
 const updating = ref(false)
-const target = ref({} as unknown as Country)
-
-onMounted(() => {
-  lang.getCountries({
-    Message: {
-      Error: {
-        Title: 'MSG_GET_COUNTRIES',
-        Message: 'MSG_GET_COUNTRIES_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    countryLoading.value = false
-  })
-})
-
-const onMenuHide = () => {
-  showing.value = false
-  target.value = {} as unknown as Country
-}
-
-const onRowClick = (country: Country) => {
-  target.value = country
-  showing.value = true
-  updating.value = true
-}
 
 const onCreate = () => {
   showing.value = true
   updating.value = false
 }
 
-const onSubmit = () => {
+const onMenuHide = () => {
+  target.value = {} as Country
   showing.value = false
+}
 
-  if (updating.value) {
-    churchLang.updateCountry({
-      Info: target.value,
-      Message: {
-        Error: {
-          Title: 'MSG_CREATE_COUNTRY',
-          Message: 'MSG_CREATE_COUNTRY_FAIL',
-          Popup: true,
-          Type: NotificationType.Error
-        }
-      }
-    }, () => {
-      // TODO
-    })
-    return
-  }
+const onRowClick = (row: Country) => {
+  target.value = { ...row }
+  updating.value = true
+  showing.value = true
+}
 
-  churchLang.createCountry({
-    Info: target.value,
+const onCancel = () => {
+  onMenuHide()
+}
+
+const onSubmit = (done: () => void) => {
+  updating.value ? updateCountry(done) : createCountry(done)
+}
+
+const createCountry = (done: () => void) => {
+  country.createCountry({
+    ...target.value,
     Message: {
       Error: {
         Title: 'MSG_CREATE_COUNTRY',
         Message: 'MSG_CREATE_COUNTRY_FAIL',
         Popup: true,
-        Type: NotificationType.Error
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_CREATE_COUNTRY',
+        Message: 'MSG_CREATE_COUNTRY_SUCCESS',
+        Popup: true,
+        Type: NotifyType.Success
       }
     }
-  }, () => {
-    // TODO
+  }, (error: boolean) => {
+    done()
+    if (error) {
+      return
+    }
+    onMenuHide()
   })
 }
 
-const onCancel = () => {
-  showing.value = false
+const updateTarget = computed(() => {
+  return {
+    ID: target?.value?.ID,
+    Country: target?.value?.Country,
+    Flag: target?.value?.Flag,
+    Short: target?.value?.Short,
+    Code: target?.value?.Code
+  } as UpdateCountryRequest
+})
+const updateCountry = (done: () => void) => {
+  country.updateCountry({
+    ...updateTarget.value,
+    Message: {
+      Error: {
+        Title: 'MSG_UPDATE_COUNTRY',
+        Message: 'MSG_UPDATE_COUNTRY_FAIL',
+        Popup: true,
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_UPDATE_COUNTRY',
+        Message: 'MSG_UPDATE_COUNTRY_FAIL',
+        Popup: true,
+        Type: NotifyType.Success
+      }
+    }
+  }, (error: boolean) => {
+    done()
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
 }
 
 const onExport = () => {
-  const blob = new Blob([JSON.stringify(
-    countries.value
-  )], { type: 'text/plain;charset=utf-8' })
-  const filename = 'countries-' +
-                   formatTime(new Date().getTime() / 1000) +
-                   '.json'
+  const blob = new Blob([JSON.stringify(countries.value)], { type: 'text/plain;charset=utf-8' })
+  const filename = 'countries-' + formatTime(new Date().getTime() / 1000) + '.json'
   saveAs(blob, filename)
+}
+
+const upload = () => {
+  // TODO
 }
 
 const loadFileButton = ref<HTMLInputElement>()
@@ -221,4 +238,29 @@ const onSubmitLoaded = () => {
   })
 }
 
+onMounted(() => {
+  if (countries.value.length === 0) {
+    getCountries(0, 100)
+  }
+})
+
+const getCountries = (offset: number, limit: number) => {
+  country.getCountries({
+    Offset: offset,
+    Limit: limit,
+    Message: {
+      Error: {
+        Title: 'MSG_GET_COUNTRIES',
+        Message: 'MSG_GET_COUNTRIES_FAIL',
+        Popup: true,
+        Type: NotifyType.Error
+      }
+    }
+  }, (error: boolean, rows: Array<Country>) => {
+    if (error || rows.length === 0) {
+      return
+    }
+    getCountries(offset + limit, limit)
+  })
+}
 </script>
