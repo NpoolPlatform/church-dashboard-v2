4a15c51905ed4b891c012cb9f0e35064614e0aba
lidaqiang <dqli161@163.com>
2022-12-16 15:52:54 +0800

    Add AppCountry

---
+++ src/components/internationalization/AppCountry.vue
@@ -0,0 +1,148 @@
+<template>
+  <q-table
+    dense
+    flat
+    :title='$t("MSG_APP_COUNTRIES")'
+    :rows='countries'
+    row-key='ID'
+    :rows-per-page-options='[10]'
+    :selected='selectedCountries'
+    selection='multiple'
+  >
+    <template #top-right>
+      <div class='row indent flat'>
+        <q-btn
+          dense
+          flat
+          class='btn flat'
+          :label='$t("MSG_AUTHORIZE")'
+          @click='onCreate'
+        />
+        <LoadingButton
+          class='dense flat btn'
+          loading
+          :label='$t("MSG_DELETE")'
+          :disable='selectedCountries.length === 0'
+          @click='onDelete'
+        />
+      </div>
+    </template>
+  </q-table>
+  <q-dialog
+    v-model='showing'
+    @hide='onMenuHide'
+    position='right'
+  >
+    <q-card class='popup-menu'>
+      <q-card-section>
+        <span>{{ $t('MSG_AUTHORIZE_COUNTRY') }}</span>
+      </q-card-section>
+      <q-card-section>
+        <CountryPicker v-model:id='countryID' />
+      </q-card-section>
+      <q-item class='row'>
+        <LoadingButton loading :label='$t("MSG_AUTHORIZE")' @click='onAuthorize' />
+        <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
+      </q-item>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang='ts'>
+import { NotifyType } from 'npool-cli-v4'
+import { appID } from 'src/api/app'
+import { getAppCountries } from 'src/api/g11n'
+import { useChurchAppCountryStore } from 'src/teststore/g11n/appcountry'
+import { AppCountry } from 'src/teststore/g11n/appcountry/types'
+import { computed, defineAsyncComponent, onMounted, ref, watch } from 'vue'
+
+const LoadingButton = defineAsyncComponent(() => import('src/components/button/LoadingButton.vue'))
+const CountryPicker = defineAsyncComponent(() => import('src/components/internationalization/CountryPicker.vue'))
+
+const country = useChurchAppCountryStore()
+const countries = computed(() => country.getCountriesByAppID(appID.value))
+
+const selectedCountries = ref([] as Array<AppCountry>)
+
+const countryID = ref('')
+const showing = ref(false)
+
+const onCreate = () => {
+  showing.value = true
+}
+
+const onMenuHide = () => {
+  showing.value = false
+}
+
+const onCancel = () => {
+  onMenuHide()
+}
+
+const onAuthorize = (done: () => void) => {
+  country.createAppCountry({
+    TargetAppID: appID.value,
+    CountryID: countryID.value,
+    Message: {
+      Error: {
+        Title: 'MSG_AUTHORIZE_COUNTRY',
+        Message: 'MSG_AUTHORIZE_COUNTRY_FAIL',
+        Popup: true,
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_AUTHORIZE_COUNTRY',
+        Message: 'MSG_AUTHORIZE_COUNTRY_SUCCESS',
+        Popup: true,
+        Type: NotifyType.Success
+      }
+    }
+  }, (error: boolean) => {
+    done()
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
+}
+
+const onDelete = (done: () => void) => {
+  country.deleteAppCountry({
+    ID: selectedCountries.value[0].ID,
+    TargetAppID: selectedCountries.value[0].AppID,
+    Message: {
+      Error: {
+        Title: 'MSG_DELETE_COUNTRY',
+        Message: 'MSG_DELETE_COUNTRY_FAIL',
+        Popup: true,
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_DELETE_COUNTRY',
+        Message: 'MSG_DELETE_COUNTRY_FAIL',
+        Popup: true,
+        Type: NotifyType.Success
+      }
+    }
+  }, (error: boolean) => {
+    done()
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
+}
+
+watch(appID, () => {
+  if (countries.value.length === 0) {
+    getAppCountries(0, 100)
+  }
+})
+
+onMounted(() => {
+  if (countries.value.length === 0) {
+    getAppCountries(0, 100)
+  }
+})
+
+</script>
---
+++ src/components/internationalization/CountryPicker.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-select
+    :disable='!updating ? false : true'
+    v-model='target'
+    :options='displayCountries'
+    options-selected-class='text-deep-orange'
+    emit-value
+    :label='myLabel'
+    map-options
+    @update:model-value='onUpdate'
+    use-input
+    @filter='onFilter'
+  >
+    <template #option='scope'>
+      <q-item v-bind='scope.itemProps'>
+        <q-item-section>
+          <q-item-label>{{ scope.opt.label }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </template>
+  </q-select>
+</template>
+<script setup lang='ts'>
+import { getCountries } from 'src/api/g11n'
+import { useChurchCountryStore } from 'src/teststore/g11n/country'
+import { computed, defineEmits, defineProps, toRef, ref, onMounted } from 'vue'
+
+interface Props {
+  id: string
+  updating?: boolean
+  label?: string,
+}
+
+const props = defineProps<Props>()
+const id = toRef(props, 'id')
+const updating = toRef(props, 'updating')
+const label = toRef(props, 'label')
+
+const myLabel = computed(() => {
+  return !label.value ? 'MSG_COUNTRIES' : label.value
+})
+
+const target = ref(id.value)
+
+const country = useChurchCountryStore()
+const countries = computed(() => Array.from(country.Countries.Countries).map((el) => {
+  return {
+    value: el.ID,
+    label: `${el.Country} | ${el.Short}`
+  }
+}))
+const displayCountries = ref(countries.value)
+
+const onFilter = (val: string, doneFn: (callbackFn: () => void) => void) => {
+  doneFn(() => {
+    displayCountries.value = countries.value.filter((el) => {
+      return el?.label?.toLowerCase().includes(val.toLowerCase())
+    })
+  })
+}
+
+const emit = defineEmits<{(e: 'update:id', id: string): void}>()
+const onUpdate = () => {
+  emit('update:id', target.value)
+}
+
+onMounted(() => {
+  if (country.Countries.Countries.length === 0) {
+    getCountries(0, 100)
+  }
+})
+</script>
