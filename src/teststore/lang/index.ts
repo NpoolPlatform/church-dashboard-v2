3b6ffed4dc8382d8e8c0a8dbede47783eea69a4f
lidaqiang <dqli161@163.com>
2022-12-22 14:40:20 +0800

    add messages

--- src/teststore/lang/index.ts
+++ src/teststore/lang/index.ts
@@ -1,16 +1,19 @@
 import { defineStore } from 'pinia'
 import { Cookies } from 'quasar'
+import { useI18n } from 'vue-i18n'
 import { AppLang } from '../g11n/applang/types'
 
 export const useLocalLangStore = defineStore('local-lang-v4', {
   state: () => ({
-    AppLang: undefined as unknown as AppLang
+    AppLang: undefined as unknown as AppLang,
+    I18n: useI18n()
   }),
   getters: {
     setLang () {
       return (lang: AppLang) => {
         this.AppLang = lang
         Cookies.set('X-Lang-ID', lang.ID, { expires: '4h', secure: true })
+        this.I18n.locale = lang.Lang
       }
     }
   },
