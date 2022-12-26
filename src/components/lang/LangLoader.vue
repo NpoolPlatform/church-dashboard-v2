f993f8c60657173160e6a257156b68fe64579042
lidaqiang <dqli161@163.com>
2022-12-26 14:04:33 +0800

    getAppMessages & AppLangs

--- src/components/lang/LangLoader.vue
+++ src/components/lang/LangLoader.vue
@@ -1,22 +1,19 @@
 <script setup lang='ts'>
 import { onMounted, computed } from 'vue'
 // import { useI18n } from 'vue-i18n'
-import { useChurchAppLangStore } from 'src/teststore/g11n/applang'
 import { AppLang } from 'src/teststore/g11n/applang/types'
-import { appID } from 'src/api/app'
-import { useChurchMessageStore } from 'src/teststore/g11n/message'
-import { getAppMessages } from 'src/api/g11n'
-import { useLocalLangStore } from 'src/teststore/lang'
+import { useAdminAppLangStore } from 'src/teststore/applang'
+import { useAdminMessageStore } from 'src/teststore/message'
+import { Message } from 'src/teststore/message/types'
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 // const { t } = useI18n({ useScope: 'global' })
 
-const lang = useChurchAppLangStore()
-const langs = computed(() => lang.getAppLangsByAppID(appID.value))
-const message = useChurchMessageStore()
-const messages = computed(() => message.getMessagesByAppID(appID.value))
+const lang = useAdminAppLangStore()
+const langs = computed(() => lang.AppLangs.AppLangs)
+const message = useAdminMessageStore()
+const messages = computed(() => message.Messages.Messages)
 
-const local = useLocalLangStore()
 onMounted(() => {
   if (langs.value.length === 0) {
     getAppLangs(0, 100)
@@ -28,16 +25,26 @@ onMounted(() => {
 
 const getAppLangs = (offset: number, limit: number) => {
   lang.getAppLangs({
-    TargetAppID: appID.value,
     Offset: offset,
     Limit: limit,
     Message: {
     }
   }, (error: boolean, rows: Array<AppLang>) => {
     if (error || rows.length === 0) {
-      if (!error) {
-        local.setLangs(langs.value)
-      }
+      return
+    }
+    getAppLangs(offset + limit, limit)
+  })
+}
+
+const getAppMessages = (offset: number, limit: number) => {
+  message.getMessages({
+    Offset: offset,
+    Limit: limit,
+    Disabled: false,
+    NotifyMessage: {}
+  }, (error: boolean, rows: Array<Message>) => {
+    if (error || rows.length === 0) {
       return
     }
     getAppLangs(offset + limit, limit)
