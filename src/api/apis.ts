a6bd0e4c7f04a8cb78430dcfdc7b7d8b627a0307
lidaqiang <dqli161@163.com>
2023-01-04 16:05:25 +0800

    add apis api

---
+++ src/api/apis.ts
@@ -0,0 +1,18 @@
+import { useChurchAPIStore } from 'src/teststore/apis'
+import { API } from 'src/teststore/apis/types'
+
+const api = useChurchAPIStore()
+export const getAPIs = (offset: number, limit: number) => {
+  api.getAPIs({
+    Exported: true,
+    Depracated: false,
+    Offset: offset,
+    Limit: limit,
+    Message: {}
+  }, (error: boolean, rows: Array<API>) => {
+    if (error || rows.length < limit) {
+      return
+    }
+    getAPIs(offset + limit, limit)
+  })
+}
