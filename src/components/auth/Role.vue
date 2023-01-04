520d1db769ddfd13081af81dd1318f58b119036e
lidaqiang <dqli161@163.com>
2023-01-04 16:32:39 +0800

    add Exported

--- src/components/auth/Role.vue
+++ src/components/auth/Role.vue
@@ -378,6 +378,16 @@ const apiColumns = computed(() => [
     field: (row: API) => row.ServiceName
   },
   {
+    name: 'Exported',
+    label: 'MSG_EXPORTED',
+    field: (row: API) => row.Exported
+  },
+  {
+    name: 'Depracated',
+    label: 'MSG_DEPRACATED',
+    field: (row: API) => row.Depracated
+  },
+  {
     name: 'CreatedAt',
     label: 'MSG_CREATED_AT',
     field: (row: API) => formatTime(row.CreatedAt)
