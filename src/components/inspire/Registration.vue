40cca733e11e88a3c7fc32d9cd9ea991ad341ef1
lidaqiang <dqli161@163.com>
2023-01-11 14:14:14 +0800

    support invitee & inviter search

--- src/components/inspire/Registration.vue
+++ src/components/inspire/Registration.vue
@@ -13,8 +13,15 @@
           dense
           flat
           class='small'
-          v-model='username'
-          :label='$t("MSG_USERNAME")'
+          v-model='invitee'
+          :label='$t("MSG_INVITEE")'
+        />
+        <q-input
+          dense
+          flat
+          class='small'
+          v-model='inviter'
+          :label='$t("MSG_INVITER")'
         />
       </div>
     </template>
@@ -37,10 +44,28 @@ const { t } = useI18n({ useScope: 'global' })
 const registration = useChurchRegistrationStore()
 const registrations = computed(() => registration.getRegistrationsByAppID(appID.value))
 
-const username = ref('')
+const invitee = ref('')
+const inviter = ref('')
 const displayRegistrations = computed(() => registrations.value.filter((el) => {
-  const _username = username.value.toLowerCase()
-  return el.InviteeEmailAddress.toLowerCase().includes(_username) || el.InviteePhoneNO.toLowerCase().includes(_username)
+  const _invitee = invitee.value.toLowerCase()
+  const _inviter = inviter.value.toLowerCase()
+  let display = true
+  if (_invitee.length > 0 && _invitee.length > 0) {
+    display = (el.InviteeEmailAddress.toLowerCase().includes(_invitee) || el.InviteePhoneNO.toLowerCase().includes(_invitee)) &&
+      (el.InviterEmailAddress.toLowerCase().includes(_inviter) || el.InviterPhoneNO.toLowerCase().includes(_inviter))
+    return display
+  }
+
+  if (_invitee.length > 0) {
+    display = el.InviteeEmailAddress.toLowerCase().includes(_invitee) || el.InviteePhoneNO.toLowerCase().includes(_invitee)
+    return display
+  }
+  if (_inviter.length > 0) {
+    display = el.InviterEmailAddress.toLowerCase().includes(_inviter) || el.InviterPhoneNO.toLowerCase().includes(_inviter)
+    return display
+  }
+
+  return display
 }))
 
 watch(appID, () => {
