cb4b18de3698e7163ea25a92fa6c69f427fa7308
jakys <jakysing@163.com>
2024-04-12 09:52:33 +0800

    Add query

--- src/components/inspire/AppCommissionConfig.vue
+++ src/components/inspire/AppCommissionConfig.vue
@@ -39,6 +39,13 @@
   >
     <template #top-right>
       <div class='row indent flat'>
+        <q-input
+          dense
+          flat
+          class='small'
+          v-model='appGoodID'
+          :label='$t("MSG_APP_GOOD_ID")'
+        />
         <q-toggle dense v-model='showAppGoodHistory' :label='$t("MSG_SHOW_HISTORY")' size='xs' />
       </div>
       <q-btn
@@ -114,6 +121,7 @@ import { computed, defineAsyncComponent, onMounted, ref, watch } from 'vue'
 import { AppID } from 'src/api/app'
 import { useI18n } from 'vue-i18n'
 import { appcommissionconfig, appgoodcommissionconfig, notify, utils } from 'src/npoolstore'
+import { ELOOP } from 'constants'
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
@@ -137,13 +145,14 @@ const displayCommissions = computed(() => commconfigs.value.filter((el) => {
   return display
 }))
 
+const appGoodID = ref('')
 const showAppGoodHistory = ref(false)
 const displayGoodCommissions = computed(() => commgoodconfigs.value.filter((el) => {
   let display = el.EndAt === 0
   if (showAppGoodHistory.value) {
-    display = el.EndAt.toString().includes('')
+    display = el.EndAt.toString().includes('') && el.AppGoodID.includes(appGoodID.value)
   }
-  return display
+  return display && el.AppGoodID.includes(appGoodID.value)
 }))
 
 const target = ref({} as appcommissionconfig.AppCommissionConfig)
