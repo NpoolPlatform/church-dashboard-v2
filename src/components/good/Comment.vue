4ff2a8e5f742a12b41b3d8bd8c9b2ad0f9c14b27
Zhao KK <kikakkz@hotmail.com>
2024-06-27 15:39:09 +0800

    Fix: http://47.116.78.131:10083/issues/3835

--- src/components/good/Comment.vue
+++ src/components/good/Comment.vue
@@ -7,7 +7,22 @@
     row-key='ID'
     :rows-per-page-options='[100]'
     @row-click='(evt, row, index) => onRowClick(row as appgoodcomment.Comment)'
-  />
+    selection='single'
+    v-model:selected='selectedComments'
+  >
+    <template #top-right>
+      <div class='row indent flat'>
+        <q-btn
+          dense
+          flat
+          class='btn flat'
+          :label='$t("MSG_DELETE")'
+          @click='onDelete'
+          :disable='selectedComment === undefined'
+        />
+      </div>
+    </template>
+  </q-table>
   <q-dialog
     v-model='showing'
     @hide='onMenuHide'
@@ -23,6 +38,10 @@
       <q-card-section>
         <span>{{ target.Content }}</span>
       </q-card-section>
+      <q-card-section>
+        <q-toggle v-model='target.Hide' :label='$t("MSG_HIDE")' />
+        <q-select :disable='!target.Hide' :options='goodbase.GoodCommentHideReasons' v-model='target.HideReason' />
+      </q-card-section>
       <q-item class='row'>
         <q-btn class='btn round' :loading='submitting' :label='$t("MSG_SUBMIT")' @click='onSubmit' />
         <q-btn class='btn alt round' :label='$t("MSG_CANCEL")' @click='onCancel' />
@@ -32,8 +51,8 @@
 </template>
 
 <script setup lang='ts'>
-import { onMounted, ref, watch } from 'vue'
-import { sdk, appgoodcomment } from 'src/npoolstore'
+import { computed, onMounted, ref, watch } from 'vue'
+import { sdk, appgoodcomment, goodbase } from 'src/npoolstore'
 
 const AppID = sdk.AppID
 const comments = sdk.goodComments
@@ -41,6 +60,8 @@ const comments = sdk.goodComments
 const showing = ref(false)
 const submitting = ref(false)
 const target = ref(undefined as unknown as appgoodcomment.Comment)
+const selectedComments = ref([] as appgoodcomment.Comment[])
+const selectedComment = computed(() => selectedComments.value[0])
 
 const onRowClick = (row: appgoodcomment.Comment) => {
   target.value = row
@@ -58,6 +79,12 @@ const onMenuHide = () => {
 const onSubmit = () => {
   sdk.adminUpdateGoodComment(target.value, () => {
     showing.value = false
+  })
+}
+
+const onDelete = () => {
+  sdk.adminDeleteGoodComment(selectedComment.value, () => {
+    selectedComments.value = []
   })
 }
 
